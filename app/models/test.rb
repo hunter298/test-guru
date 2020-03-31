20644c37e656125f1112d1acd9e1b300ce04ac49
hunter298 <anton-volianskyi@ukr.net>
2020-03-31 22:40:05 +0300

    lesson4 MORE corrections

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,7 +1,5 @@
 class Test < ApplicationRecord
-  belongs_to :category
-
-  def self.sort_reverse category
-    Test.joins("JOIN categories ON tests.category_id = categories.id and categories.title = '#{category}'").order(title: :desc).pluck(:title)
+  def self.sort_reverse(category)
+    joins("JOIN categories ON tests.category_id = categories.id").where("categories.title = :category", category: category).order(title: :desc).pluck(:title)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
 
   def tests_by_level level
-    Test.joins("JOIN results ON tests.id = results.test_id and results.user_id = #{self.id}").where(level: level).pluck(:title)
+    Test.joins("JOIN results ON results.test_id = tests.id").where("results.user_id = :user and level = :level", user: id, level: level)
   end
 end
