a02aadff2c00b77ce31999426d594591d9984e2d
hunter298 <46868577+hunter298@users.noreply.github.com>
2020-04-01 20:01:48 +0300

    Update app/models/test.rb
    Co-Authored-By: max-underthesun <max.underthesun@gmail.com>

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,5 +1,5 @@
 class Test < ApplicationRecord
   def self.sort_reverse(category)
-    joins("JOIN categories ON tests.category_id = categories.id").where("categories.title = :category", category: category).order(title: :desc).pluck(:title)
+    joins("JOIN categories ON tests.category_id = categories.id").where(categories: { title: category }).order(title: :desc).pluck(:title)
   end
 end
