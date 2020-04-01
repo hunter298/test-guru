ca67e2d3ff123c14cfab5f63baaa6fa683e9f93c
hunter298 <46868577+hunter298@users.noreply.github.com>
2020-04-01 20:02:10 +0300

    Update app/models/user.rb
    Co-Authored-By: max-underthesun <max.underthesun@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
 
   def tests_by_level(level)
-    Test.joins("JOIN results ON results.test_id = tests.id").where("results.user_id = :user and level = :level", user: id, level: level)
+    Test.joins("JOIN results ON results.test_id = tests.id").where(results: { user_id: id }, level: level)
   end
 end
