a20f5633800a4028aa4187008c962edf7c2959d9
hunter298 <anton-volianskyi@ukr.net>
2020-04-02 21:56:48 +0300

    lesson5 final

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,11 +1,12 @@
 class Test < ApplicationRecord
-  belongs_to :category
+  belongs_to :category, optional: true
   belongs_to :creator, class_name: "User"
   has_many :questions, dependent: :destroy
   has_many :results, dependent: :destroy
   has_many :users, through: :results
 
   def self.sort_reverse(category)
-    Category.find_by(title: category).tests.order(title: :desc).pluck(:title)
+    # Category.find_by(title: category).tests.order(title: :desc).pluck(:title)
+    joins(:category).where(categories: { title: category }).order(title: :desc).pluck(:title)
   end
 end
