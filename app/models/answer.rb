5ca8c55eec8ffba4410f00013cab0b9ae61022b0
hunter298 <anton-volianskyi@ukr.net>
2020-04-12 20:57:32 +0300

    lesson9 after corrections

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,6 +1,6 @@
 class Answer < ApplicationRecord
   belongs_to :question
-  scope :right, -> { where(correct: true) }
+  scope :correct, -> { where(correct: true) }
   validates :body, presence: true
   validate :validate_max_answers, on: :create
   # validate :validate_minimum_answers, on: :destroying???????
--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -3,13 +3,10 @@ class TestPassage < ApplicationRecord
   belongs_to :test
   belongs_to :current_question, class_name: 'Question', optional: true
 
-  before_validation :before_validation_set_first_question, on: :create
-  before_save :before_save_next_question, unless: :new?
+  before_validation :before_validation_set_current_question
 
   def accept!(answer_ids)
-    if correct_answer?(answer_ids)
-      self.correct_questions += 1
-    end
+    self.correct_questions += 1 if correct_answer?(answer_ids)
 
     save!
   end
@@ -22,37 +19,39 @@ class TestPassage < ApplicationRecord
     ((correct_questions.to_f / test.questions.count) * 100).round
   end
 
-  def success_class
-    success_rate >= 85 ?  'successfull' : 'unsuccessfull'
+  def successful?
+    success_rate >= 85
   end
 
   def question_count
-    hash = {}
-    test.questions.ids.each.with_index(1) { |id, index| hash[id] = index}
-    "#{hash[current_question.id]} of #{test.questions.count}"
+    test.questions.order(:id).where('id <= :current', current: current_question.id).count
   end
 
   private
 
-  def before_validation_set_first_question
-    self.current_question = test.questions.first if test.present?
+  def before_validation_set_current_question
+    self.current_question = next_question
+  end
+
+  def next_question
+    if current_question.nil?
+      test.questions.order(:id).first
+    else
+      test.questions.order(:id).where('id > :current', current: current_question.id).first
+    end
+
   end
 
   def correct_answer?(answer_ids)
-    correct_answers.ids.sort == answer_ids.map(&:to_i).sort
+    if answer_ids.nil?
+      correct_answers.empty?
+    else
+      correct_answers.ids.sort == answer_ids.map(&:to_i).sort
+    end
   end
 
   def correct_answers
-    current_question.answers.right
+    current_question.answers.correct
   end
-
-  def before_save_next_question
-    self.current_question = test.questions.order(:id).where('id > :current', current: current_question.id).first
-  end
-
-  def new?
-    self.new_record?
-  end
-
 
 end
