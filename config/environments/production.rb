f11627d264e840bc4b697b6e8a9297bd1b1e90d9
hunter298 <anton-volianskyi@ukr.net>
2020-05-06 00:30:51 +0300

    configuring env variables for actionmailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,8 +66,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
       adress: 'smtp.gmail.com',
       port: 587,
-      user_name: ENV[SMTP_USERNAME],
-      password: ENV[SMTP_PASSWORD],
+      user_name: ENV['SMTP_USERNAME'],
+      password: ENV['SMTP_PASSWORD'],
       authentication: 'plain',
       enable_startls_auto: true }
 
